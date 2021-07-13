dcb428420964c3bc24668407880a6955199d8fdc
Serse12 <antonio.allocca12345@live.it>
2021-07-13 11:24:39 +0200

    Initial Commit

---
+++ ipong/ipongApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipongApp.swift
+//  ipong
+//
+//  Created by Antonio Allocca on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ipongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
